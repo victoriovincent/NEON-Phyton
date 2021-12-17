e79dff9515a80a3c073b195dd89acbb1ed347dd6
victoriovincent <victorio.vincent@yahoo.com>
2021-12-17 16:22:46 +0700

    upload

---
+++ sesi1/app.py
@@ -0,0 +1,4 @@
+name = "Hacktiv8"
+Age = 54
+has_laptops = True
+print(name, Age, has_laptops)
---
+++ sesi2/sesi2.py
@@ -0,0 +1,6 @@
+d = {'foo': 1, 'bar': 2, 'baz': 3}
+for k, v in d.items():  # -->  (value1, value2)
+                        #       'foo', 1
+                        #       'bar', 2
+                        #       'baz', 3
+    print(k, ":", v)
